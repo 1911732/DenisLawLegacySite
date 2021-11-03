c54f98ad49f0128c10caa13637db41cc5b0c952e
JONATHAN DUFFY <1911732@rgu.ac.uk>
2021-11-03 14:50:34 +0000

    ...

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 //server side stuff
 app.set('view engine', 'ejs');
-app.set('Views', __dirname + '/Views')
+app.set('views', __dirname + '/views')
 app.listen(8080)
 
 //Default page
